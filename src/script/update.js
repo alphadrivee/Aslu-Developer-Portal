a27f8c0273d9dba38878b9edc5913d9e20f173a7
Arcle <38045087+alphadrivee@users.noreply.github.com>
2021-01-27 23:38:48 +0500

    Updater Feature

--- src/script/update.js
+++ src/script/update.js
@@ -4,8 +4,5 @@ const label = document.getElementById("label");
 const progress = document.getElementById("progress");
 
 ipcRenderer.on('message', function(event, text) {
-    var container = document.getElementById('messages');
-    var message = document.createElement('div');
-    message.innerHTML = text;
-    container.appendChild(message);
+    label.textContent = text;
   });
